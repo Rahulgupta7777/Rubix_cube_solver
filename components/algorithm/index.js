bcf50f246e5d9fe4983f31f9b7433b42338acf63
Rahul Gupta <rahulgupta193246@gmail.com>
2025-11-27 12:49:04 +0000

    Add algorithm components: AlgorithmCard, AlgorithmList, AlgorithmSearch
    Co-authored-by: Rahulgupta7777 <184137065+Rahulgupta7777@users.noreply.github.com>

---
+++ components/algorithm/index.js
@@ -0,0 +1,9 @@
+import AlgorithmCard from './AlgorithmCard';
+import AlgorithmList from './AlgorithmList';
+import AlgorithmSearch from './AlgorithmSearch';
+
+export {
+  AlgorithmCard,
+  AlgorithmList,
+  AlgorithmSearch,
+};
