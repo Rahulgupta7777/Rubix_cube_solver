ac827f26d3d4516ffbab293a0c6b98b416169026
Rahul Gupta <rahulgupta193246@gmail.com>
2025-11-27 12:47:30 +0000

    Add context providers: SettingsContext, CubeContext
    Co-authored-by: Rahulgupta7777 <184137065+Rahulgupta7777@users.noreply.github.com>

---
+++ context/index.js
@@ -0,0 +1,9 @@
+import { SettingsProvider, useSettings } from './SettingsContext';
+import { CubeProvider, useCube } from './CubeContext';
+
+export {
+  SettingsProvider,
+  useSettings,
+  CubeProvider,
+  useCube,
+};
